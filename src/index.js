a49f67a0be69057836ce8891d2b0eca69153cdf6
matheussvini <matheussvini@outlook.com>
2022-11-14 14:46:20 -0300

    Create requisitions get and post of route /messages

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-import express from "express";
+import express, { application } from "express";
 import { MongoClient, ObjectId } from "mongodb";
 import dotenv from "dotenv";
 import cors from "cors";
@@ -8,6 +8,12 @@ import dayjs from "dayjs";
 
 const userSchema = joi.object({
   name: joi.string().min(1).required(),
+});
+
+const messageSchema = joi.object({
+  to: joi.string().min(1).required(),
+  text: joi.string().min(1).required(),
+  type: joi.string().valid("message", "private_message"),
 });
 
 const app = express();
@@ -58,9 +64,9 @@ app.post("/participants", async (req, res) => {
       type: "status",
       time: dayjs(currentTime).format("HH:mm:ss"),
     };
-    
+
     await messagesCollection.insertOne(message);
-    res.sendStatus(201);
+    res.status(201).send("Usuário cadastrado com sucesso!");
   } catch (err) {
     res.status(500).send(err);
   }
@@ -70,6 +76,97 @@ app.get("/participants", async (req, res) => {
   try {
     const participants = await usersCollection.find({}).toArray();
     res.status(200).send(participants);
+  } catch (err) {
+    res.status(500).send(err);
+  }
+});
+
+app.post("/messages", async (req, res) => {
+  const messageBody = req.body;
+  const user = req.headers.user;
+
+  if (!user) {
+    return res.status(422).send({
+      message: 'Por favor envie um header com campo "nome" informando o nome do usuário',
+    });
+  }
+
+  try {
+    const userExists = await usersCollection.findOne({ name: user });
+    if (!userExists) {
+      return res.status(422).send({
+        message:
+          "Não existe nenhum usuário com este nome, por favor escolha um usuário que já esteja cadastrado!",
+      });
+    }
+
+    const { error } = messageSchema.validate(messageBody, {
+      abortEarly: false,
+    });
+    if (error) {
+      const arrErrors = error.details.map((e) => e.message);
+      return res.status(422).send(arrErrors);
+    }
+
+    const currentTime = Date.now();
+    const message = {
+      ...messageBody,
+      from: user,
+      time: dayjs(currentTime).format("HH:mm:ss"),
+    };
+
+    await messagesCollection.insertOne(message);
+    res.status(201).send("Mensagem enviada com sucesso!");
+  } catch (err) {
+    res.status(500).send(err);
+  }
+});
+
+app.get("/messages", async (req, res) => {
+  const user = req.headers.user;
+  const limit = parseInt(req.query.limit);
+
+  if (!user) {
+    return res.status(422).send({
+      message: 'Por favor envie um header com campo "nome" informando o nome do usuário',
+    });
+  }
+
+  try {
+    const userExists = await usersCollection.findOne({ name: user });
+    if (!userExists) {
+      return res.status(422).send({
+        message:
+          "Não existe nenhum usuário com este nome, por favor escolha outro nome!",
+      });
+    }
+
+    if (limit <= 0) {
+      return res
+        .status(400)
+        .send("Informe um limite válido maior ou igual a 1");
+    }
+
+    let arrMessages = await messagesCollection
+      .find({
+        $or: [{ to: "Todos" }, { to: user }, { from: user }],
+      })
+      .sort({ time: -1 })
+      .toArray();
+
+    if (!limit || limit > arrMessages.length) {
+      return res.status(200).send(arrMessages);
+    }
+
+    arrMessages = await messagesCollection
+      .find({
+        $or: [{ to: "Todos" }, { to: user }, { from: user }],
+      })
+      .sort({ time: -1 })
+      .limit(limit)
+      .toArray();
+
+    res.status(200).send(arrMessages);
   } catch (err) {
     res.status(500).send(err);
   }
