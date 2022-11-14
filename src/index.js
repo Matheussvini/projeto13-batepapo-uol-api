b5f7239dac447e5566879166e1e8dc3443b1e7ba
matheussvini <matheussvini@outlook.com>
2022-11-14 10:57:36 -0300

    Finish initial configs

--- src/index.js
+++ src/index.js
@@ -1,21 +1,32 @@
-import express from 'express'
-import { MongoClient, ObjectId } from 'mongodb'
-import dotenv from 'dotenv'
-import cors from 'cors'
-import joi from 'joi'
+import express from "express";
+import { MongoClient, ObjectId } from "mongodb";
+import dotenv from "dotenv";
+import cors from "cors";
+import joi from "joi";
 
+const app = express();
 dotenv.config();
+app.use(cors());
+app.use(express.json());
 
 const mongoClient = new MongoClient(process.env.MONGO_URI);
 let db;
 
 try {
-    await mongoClient.connect();
-    db = mongoClient.db('DIRETORIOOOO')
+  await mongoClient.connect();
 } catch (err) {
-    console.log("Erro no mongo.conect", err)
+  console.log("Erro no mongo.conect", err);
 }
 
-const app = express();
-app.use(cors());
-app.use(express.json());
+db = mongoClient.db("DIRETORIOOOO");
+const talCollection = db.collection("COLLECTIONNNNN");
+
+// ROTAS:
+
+
+
+
+
+
+const port = 5000;
+app.listen(port, () => console.log(`Server running in port: ${port}`));
